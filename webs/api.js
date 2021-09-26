9e401955532ec649305e76788aeef3864a5d57d9
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-09-26 20:32:21 +0200

    remote bot controll

--- webs/api.js
+++ webs/api.js
@@ -5,16 +5,27 @@ const bot = require('../hrcx/bot')
 const router = express.Router();
 
 router.get('/start', (req, res) => {
-    
-    res.status(200).json({status: "Started"});
+    bot.start().then(() => {
+        console.log(`[BOT] Discord instance started`);
+        res.status(200).json({message: "Discord instance started", status: 200});
+    }).catch((err) => {
+        res.status(500).json({message: `Could not start discord instance: ${err}`, status: 500});
+    });
 });
 
 router.get('/stop', (req, res) => {
-    res.status(200).json({status: "stopped"});
+    bot.stop().then(() => {
+        console.log(`[BOT] Discord instance stopped`);
+        res.status(200).json({message: "Discord instance stopped", status: 200});
+    }).catch((err) => {
+        res.status(500).json({message: `Could not stop discord instance: ${err}`, status: 500});
+    });
 });
 
 router.get('/info', (req, res) => {
     res.status(200).json({info: "info"});
 });
+
+router.all('/*', (req, res) => res.status(404).json({message: "Page not found", status: 404}));
 
 module.exports = router;
