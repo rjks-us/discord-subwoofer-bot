598afdce9c94825518c9ab5194c6351109bfcade
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-03 18:16:17 +0200

    searches youtube api

--- hrcx/track/lib.js
+++ hrcx/track/lib.js
@@ -1,13 +1,37 @@
 const yts = require( 'yt-search' )
 
-const search = (query) => {
-    return new Promise(async (resolve, rejects) => {
-        const result = await yts(query);
-        if(result.videos.length == 0) return rejects('Invalid query search');
+/**
+ * 
+ * @description Returns the best result for your search query from youtube
+ * @param {String} query 
+ * @returns Video element
+ */
+const search = async (query) => {
+    return await new Promise(async (resolve, rejects) => {
+        //catch youtube summon from search, prevent invalid searches, do not change
+        const result = await yts(query.replace('https://youtube.com/watch?v=', ''));
+        if(result.videos === null || result.videos.length == 0) return rejects('Invalid query search');
         resolve(result.videos[0]);
     });
 }
 
+/**
+ * 
+ * @description Returns the amount of aviable elements returned by the youtube api
+ * @param {String} query 
+ * @param {Int} amount
+ * @returns Array of video elements
+ */
+const searchArray = async (query, amount) => {
+    return await new Promise(async (resolve, rejects) => {
+        const result = await yts(query);
+        if(result.videos === null || result.videos.length == 0) return rejects('Invalid query search');
+        if(result.videos.length >= amount) return resolve(result.videos.slice(0, amount));
+        resolve(result.videos.slice(0, result.videos.length));
+    });
+}
+
 module.exports = {
-    search: search
+    search: search,
+    searchArray: searchArray
 }
