9b454967cda1c77b7cf43016d3a748e5d312a90b
robert-kratz <60996025+robert-kratz@users.noreply.github.com>
2021-10-03 18:16:43 +0200

    search button implemented

--- hrcx/event/btn.js
+++ hrcx/event/btn.js
@@ -1,8 +1,41 @@
+const track = require('../track/trackmanager');
+const lib = require('../track/lib');
+
 module.exports = {
 	name: 'interactionCreate',
 	once: false,
 	async execute(interaction) {
         if (!interaction.isButton()) return;
         
+		if(interaction.customId.startsWith('summon-song:')) {
+
+			var url = interaction.customId.replaceAll('summon-song:', '');
+
+			const vc = interaction.member.voice.channel;
+
+			if(!vc) return interaction.reply({content: ':name_badge: You have to be in a voice channel to execute this command.', ephemeral: true,});
+	
+			const perm = vc.permissionsFor(interaction.client.user);
+			if(!perm.has('CONNECT') || !perm.has('SPEAK')) return interaction.reply({content: ':name_badge: I do not have the permission to connect or speak in your channel.', ephemeral: true,});
+
+			lib.search(url).then((vid) => {
+
+				track.play(interaction.guild, vc, vid, interaction, (result) => {
+					if(result.joined && result.playing) return interaction.reply({content: ':inbox_tray: I have successfully connected to your voice channel!\n' +
+						':musical_keyboard: Playing ``' + vid.title + '`` from ' + vid.author.name, ephemeral: true});
+
+					if(result.addedToQueue) return interaction.reply({content: ':ladder: ``' + vid.title + '`` from ' + vid.author.name + ' was added to your song queue', ephemeral: true});
+					
+					if(result.playing) return interaction.reply({content: ':musical_keyboard: Playing ``' + vid.title + '`` from ' + vid.author.name, ephemeral: true});
+				});
+				
+			}).catch((err) => {
+				console.log(err);
+				return interaction.reply({content: ':name_badge: Could not find any song for ``' + url + '``', ephemeral: true});
+			})
+		}
+
+		//console.log(interaction);
+
 	},
 };
